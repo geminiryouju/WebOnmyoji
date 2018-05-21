ed1cea769bc293028d5ac406ae175380102d6a4c
Ryou <geminiryouju@gmail.com>
2018-05-21 16:31:47 +0700

    new

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width = device-width, initial-scale = 1">
-		<title>Bootstrap Tutorial</title>
+		<title>Onmyoji Viet Nam</title>
+		<link rel="shortcut icon" type='image/x-icon' href='images/favicon.ico' />
 		<link rel="stylesheet" type="text/css" href="admin/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="admin/css/style.css">
 		
@@ -18,28 +19,7 @@
 		  <source src="admin/videos/video.mp4" type="video/mp4"> <!--link video-->
 		</video>
 		
-		<!-- Navbar -->
-		<nav class="navbar nav_top">
-			<div class="container-fluid">
-				<div class="navbar-header">
-				  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span> 
-				  </button>
-				</div>
-				<div class="collapse navbar-collapse" id="myNavbar">
-					<ul class="nav navbar-nav">
-					  <li class="active"><a href="trangchu.html">Trang chủ</a></li>
-					  <li><a href="#">Shikigami</a></li>
-					  <li><a href="#">Trang bị</a></li>
-					  <li><a href="#">Phép</a></li>
-					  <li><a href="#">Ngọc</a></li>
-					  <li><a href="#">OnmyojiTV</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav> <!-- End navbar -->
+		<?php include "lib/menu.php"; ?>
 		
 		<!-- BG img in small screen -->
 		<div class="visible-xs bg_img"></div>
---
+++ lib/menu.php
@@ -0,0 +1,27 @@
+<?php
+	
+?>
+
+<!-- Navbar -->
+<nav class="navbar nav_top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span> 
+		  </button>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav">
+			  <li class="active"><a href="index.html">Trang chủ</a></li>
+			  <li><a href="#">Shikigami</a></li>
+			  <li><a href="#">Trang bị</a></li>
+			  <li><a href="#">Phép</a></li>
+			  <li><a href="#">Ngọc</a></li>
+			  <li><a href="#">OnmyojiTV</a></li>
+			  <li><a href="#">Liên hệ</a></li>
+			</ul>
+		</div>
+	</div>
+</nav> <!-- End navbar -->
